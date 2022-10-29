0c7f86717aeb3180691b0aa288e521f9842d4823
Manuel Muñoz Solera <mamuso@mamuso.net>
2022-10-29 00:02:14 -0700

    Basic action scaffold (#15)
    Creating basic functions for the action

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,87 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {graphql} from '@octokit/graphql'
+import {NodeHtmlMarkdown} from 'node-html-markdown'
+import type {GraphQlQueryResponseData} from '@octokit/graphql'
+
+const discussion_url = core.getInput('discussion-url', {required: true})
+const token = core.getInput('token', {required: true})
+
+const graphqlWithAuth = graphql.defaults({
+  headers: {
+    authorization: `token ${token}`
+  }
+})
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const url = new URL(discussion_url)
+    const [, owner, repo, , id] = url.pathname.split('/')
+    const discussion_number: number = parseInt(id, 10)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const query = `
+      query ($owner: String!, $repo: String!, $discussion_number: Int!) {
+        repository(owner: $owner, name: $repo) {
+          id
+          discussion(number: $discussion_number) {
+            id
+            title
+            createdAt
+            bodyHTML
+            author {
+              login
+            }
+            comments(first: 100) {
+              nodes {
+                id
+                createdAt
+                bodyHTML
+                author {
+                  login
+                }
+                replies(first: 100) {
+                  nodes {
+                    id
+                    createdAt
+                    bodyHTML
+                    author {
+                      login
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `
 
-    core.setOutput('time', new Date().toTimeString())
+    const data: GraphQlQueryResponseData = await graphqlWithAuth(query, {
+      owner,
+      repo,
+      discussion_number
+    })
+    const discussion = data.repository.discussion
+
+    // shape the markdown
+    let md = `# ${discussion.title}\n`
+    md += `from ${discussion.author.login} on ${discussion.createdAt}\n\n`
+    md += `${NodeHtmlMarkdown.translate(discussion.bodyHTML)}\n\n`
+    md += `---\n\n`
+
+    for (const comment of discussion.comments.nodes) {
+      md += `## ${comment.author.login} on ${comment.createdAt}\n\n`
+      md += `${NodeHtmlMarkdown.translate(comment.bodyHTML)}\n\n`
+      md += `---\n\n`
+      for (const replies of comment.replies.nodes) {
+        md += `### ${replies.author.login} on ${replies.createdAt}\n\n`
+        md += `${NodeHtmlMarkdown.translate(replies.bodyHTML)}\n\n`
+        md += `---\n\n`
+      }
+    }
+
+    // Deliver the output
+    core.setOutput('markdown', md)
+    core.summary.addRaw(md).write()
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
